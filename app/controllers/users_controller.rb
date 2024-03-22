1f831369f714b916dac0d46d85840060ee930c0b
Topaz712 <tzam712@gmail.com>
2024-03-21 21:24:02 -0500

    ADD: index action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+
+  def index
+    users = UserService::Base.filter_users(params)
+    render json: users, status: :ok
+  end
+
   def create
     user = UserService::Base.create_user(user_params)
     if user.valid?
