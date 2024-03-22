e970efb0e8c00b30fe1092660e9b7093259f4623
Topaz712 <tzam712@gmail.com>
2024-03-21 21:31:11 -0500

    ADD: seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create(name: 'John Doe', email: 'johndoe123@gmail.com');
+User.create(name: 'Jane Doe', email: 'janedoe123@gmail.com');
+User.create(name: 'Another Jane Doe', email: 'anotherjanedoe123@gmail.com');
