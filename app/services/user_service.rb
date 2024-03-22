732d49a81f82f11389670c92e4e611a8bd9c5a97
Topaz712 <tzam712@gmail.com>
2024-03-21 21:36:03 -0500

    REFACTOR: UserService to use ServiceContract

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -2,18 +2,23 @@ module UserService
   module Base
     def self.create_user(params)
       user = User.new(params)
-      if user.save
-        user
-      else
-        user.errors
-      end
-    end
+
+      begin
+        # are there any db/model errors?
+       user.save!
+     rescue ActiveRecord::RecordInvalid => exception
+       # return an error instance
+       return ServiceContract.error(user.errors.full_messages) unless user.valid?
+     end
+
+     ServiceContract.success(user)
+   end
 
     def self.filter_users(params)
       users = User.all
       users = users.where(name: params[:name]) if params[:name].present?
       users = users.where(email: params[:email]) if params[:email].present?
-      users
+      ServiceContract.success(users)
     end
   end
 end
